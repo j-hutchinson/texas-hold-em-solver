65ab8f6f3ec2400d5d4c6b7ca50efbce4b6254ff
Jack Hutchinson <jhutchinson@elsevier.com>
2020-11-02 17:42:11 +0000

    Add comparison logic

--- src/PokerHand.js
+++ src/PokerHand.js
@@ -1,15 +1,72 @@
-export class PokerHand {
-
-	compareWith() {
-		return Result.TIE;
-	}
-
-}
-
 export const Result = {
 	WIN: 1,
 	LOSS: 2,
 	TIE: 3
 };
+
+export class PokerHand {
+	constructor(hand) {
+		this.hand = hand;
+	}
+
+	scoreHand(hand) {
+		const cards = hand.split(' ');
+		const order = '23456789TJQKA';
+
+		const faces = cards
+			.map((card) => String.fromCharCode([77 - order.indexOf(card[0])]))
+			.sort();
+		const suits = cards.map((card) => card[1]).sort();
+		const counts = faces.reduce(count, {});
+		const duplicates = Object.values(counts).reduce(count, {});
+		const flush = suits[0] === suits[4];
+		const straight = faces.every((face, index) => face.charCodeAt(0) - faces[0].charCodeAt(0) === index);
+		let rank =
+			(flush && straight && 1) ||
+			(duplicates[4] && 2) ||
+			(duplicates[3] && duplicates[2] && 3) ||
+			(flush && 4) ||
+			(straight && 5) ||
+			(duplicates[3] && 6) ||
+			(duplicates[2] > 1 && 7) ||
+			(duplicates[2] && 8) ||
+			9;
+
+		function sortByCountFirst(a, b) {
+			const countDiff = counts[b] - counts[a];
+			if (countDiff) return countDiff;
+			return b > a ? -1 : b === a ? 0 : 1;
+		}
+
+		function count(c, a) {
+			c[a] = (c[a] || 0) + 1;
+			return c;
+		}
+
+		return {
+			rank,
+			value: faces.sort(sortByCountFirst).join('')
+		};
+	}
+
+	compareWith({ hand }) {
+		let scoredHand1 = this.scoreHand(this.hand);
+		let scoredHand2 = this.scoreHand(hand);
+
+		if (scoredHand1.rank === scoredHand2.rank) {
+			if (scoredHand1.value < scoredHand2.value) {
+				return Result.WIN;
+			} else if (scoredHand1.value > scoredHand2.value) {
+				return Result.LOSS;
+			} else {
+				return Result.TIE;
+			}
+		}
+		if (scoredHand1.rank < scoredHand2.rank) {
+			return Result.WIN;
+		}
+		return Result.LOSS;
+	}
+}
 
 export default PokerHand;
--- src/PokerHand.test.js
+++ src/PokerHand.test.js
@@ -1,18 +1,20 @@
 import PokerHand, { Result } from './PokerHand.js';
 
 describe('PokerHand', () => {
-
 	describe('compareWith()', () => {
+		test.each`
+			cards1              | cards2              | winningHand                    | outcome
+			${'AC KS QS JH TH'} | ${'4S 5S 8C AS AD'} | ${'Royal flush'}               | ${Result.WIN}
+			${'AC AH QS QH 4H'} | ${'4S 5S 8C AS AD'} | ${'2 pair'}                    | ${Result.WIN}
+			${'2C 9S 4S 5H 6H'} | ${'3S 5S 6C 9S TD'} | ${'Highcard'}                  | ${Result.LOSS}
+			${'AC 4S 5S 8C AH'} | ${'4S 5S 8C AS AD'} | ${'Same hands'}                | ${Result.TIE}
+			${'AC AS AH AC 4H'} | ${'4S 4S 4C 4S 2D'} | ${'4 of a kind w/ highcard'}   | ${Result.WIN}
+			${'AC AS AH AC 4H'} | ${'4S 4S 4C 3S 2D'} | ${'4 of a kind > 3 of a kind'} | ${Result.WIN}
+		`(`$winningHand`, ({ cards1, cards2, outcome }) => {
+			const hand1 = new PokerHand(cards1);
+			const hand2 = new PokerHand(cards2);
 
-		it(`ties`, () => {
-
-			const hand1 = new PokerHand('AC 4S 5S 8C AH');
-			const hand2 = new PokerHand('4S 5S 8C AS AD');
-
-			expect(hand1.compareWith(hand2)).toBe(Result.TIE);
-
+			expect(hand1.compareWith(hand2)).toBe(outcome);
 		});
-
 	});
-
 });
